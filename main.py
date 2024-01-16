f376aa2ab750c555e9addc521ba8c590bf51787b
ElgaLanazo <elga.cuenta@gmail.com>
2024-01-16 10:54:24 -0600

    cleaned the main script

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from fastapi import FastAPI
 from mangum import Mangum
 from routers.labyrinthRouter import lab
@@ -7,16 +6,6 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.include_router(lab)
 handler = Mangum(app)
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 def read_root():
