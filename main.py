1fdd0a0d8c24002a466ef44acac377bb7d3cec7b
ElgaLanazo <elga.cuenta@gmail.com>
2024-01-16 14:36:17 -0600

    Added Success Message

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from routers.labyrinthRouter import lab
-from fastapi.middleware.cors import CORSMiddleware
+from Class.Responses import Response
 
 app = FastAPI()
 app.include_router(lab)
@@ -9,4 +9,4 @@ handler = Mangum(app)
 
 @app.get("/")
 def read_root():
-    return {"Hello World"}
+    return Response.success(message="Connection Successfull")
